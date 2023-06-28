aace78c0cf5ee43d0c2a0e22ea5749b6a100b3ec
ShawnGao <2519306859@qq.com>
2023-06-28 18:54:12 +0800

    :art: Modify the code format and delete useless code.

--- lib/db/sqlite/cache.dart
+++ lib/db/sqlite/cache.dart
@@ -2,23 +2,9 @@ import 'package:cloud_lock/db/sqlite.dart';
 
 class Cache {
 
-  static const String tableName = "`cache`";
-  static const String keyField = "`key`";
-  static const String valueField = "`value`";
-  static const String sql = "CREATE TABLE IF NOT EXISTS $tableName ("
-      "  $keyField varchar(64) NOT NULL PRIMARY KEY,"
-      "  $valueField varchar(255) NOT NULL"
-      ")";
-
-  Cache() {
-    _initAsync().then((_) {
-      print("Initialization complete.");
-    });
-  }
-
-  Future<void> _initAsync() async {
-    (await Sqlite.instance.database).rawQuery(sql);
-  }
+  static const String tableName = Sqlite.tableName;
+  static const String keyField = Sqlite.keyField;
+  static const String valueField = Sqlite.valueField;
 
   Future<int> set(String key, String value) async {
     final db = await Sqlite.instance.database;
@@ -31,23 +17,13 @@ class Cache {
     return await db.delete(tableName, where: "$keyField = ?", whereArgs: [key]);
   }
 
-  Future<String?> get(String key) async {
+  Future<String> get(String key) async {
     final db = await Sqlite.instance.database;
     var result = await db.rawQuery('SELECT $valueField FROM $tableName WHERE $keyField = ?', [key]);
     if (result.isNotEmpty) {
       return result.first['value'].toString();
     } else {
-      return null;
+      return '';
     }
-  }
-
-  Future<void> displayAll() async {
-    final db = await Sqlite.instance.database;
-    (await db.rawQuery("SELECT * FROM $tableName"))
-    .toList().forEach((element) {
-      var tmpKey = element['key'];
-      var tmpValue = element['value'];
-      print("Key: $tmpKey, Value: $tmpValue");
-    });
   }
 }
