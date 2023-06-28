cb08c040c7a6a419319d28387073441b3024ff59
ShawnGao <2519306859@qq.com>
2023-06-28 19:00:03 +0800

    :sparkles: Added loading component.

--- lib/main.dart
+++ lib/main.dart
@@ -25,9 +25,10 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       title: appName,
-      home: LoginScreen(),
+      home: const LoginScreen(),
+      builder: EasyLoading.init(),
     );
   }
 }
