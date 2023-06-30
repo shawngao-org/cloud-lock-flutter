bc2a813365dbd18824f52d1c44555a42c4afb0f8
ShawnGao <2519306859@qq.com>
2023-06-30 16:02:29 +0800

    :sparkles: Request to ignore battery policy.

--- android/app/src/main/kotlin/org/shawngao/cloud_lock/MainActivity.kt
+++ android/app/src/main/kotlin/org/shawngao/cloud_lock/MainActivity.kt
@@ -1,8 +1,13 @@
 package org.shawngao.cloud_lock
 
+import android.annotation.SuppressLint
+import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Build
+import android.os.PowerManager
+import android.provider.Settings
 import androidx.annotation.RequiresApi
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
@@ -33,7 +38,28 @@ class MainActivity: FlutterActivity() {
             ActivityCompat.requestPermissions(this, unGrantPermissions.toTypedArray(),
                 1)
         }
+        if (!isIgnoreBatteryOptimizations()) {
+            requestIgnoreBatteryOptimizations()
+        }
         super.configureFlutterEngine(flutterEngine)
+    }
+
+    @RequiresApi(Build.VERSION_CODES.M)
+    fun isIgnoreBatteryOptimizations(): Boolean {
+        val pm = getSystemService(Context.POWER_SERVICE) as PowerManager
+        return pm.isIgnoringBatteryOptimizations(packageName)
+    }
+
+    @SuppressLint("BatteryLife")
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    fun requestIgnoreBatteryOptimizations() {
+        try {
+            val intent = Intent(Settings.ACTION_REQUEST_IGNORE_BATTERY_OPTIMIZATIONS)
+            intent.data = Uri.parse("package:$packageName")
+            startActivity(intent)
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
     }
 
     @Deprecated("Deprecated in Java")
